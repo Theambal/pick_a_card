06be89d103656da3f59c5260c5d787f28ce46615
Andrey <theambal@gmail.com>
2021-04-03 22:13:31 +0300

    added take some cards

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,17 @@
 values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 suits = ["♦", "♠", "♥", "♣"] # Буби, Пики, Черви, Крести
 
-# Выведем произвольную карты, выбрав по одному элементу из массивов
-puts "#{values.sample}#{suits.sample}"
+cards = []
+
+values.each do |value|
+  suits.each do |suit|
+    cards << "#{value} of #{suit}"
+  end
+end
+
+cards.shuffle!
+
+puts 'Сколько карт вам взять?'
+number = $stdin.gets.to_i
+
+number.times {puts cards.pop }
